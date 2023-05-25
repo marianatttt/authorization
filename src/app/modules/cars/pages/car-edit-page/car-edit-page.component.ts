f3fc7aae3f0bc2d4040bb959074df71aa3977135
marianatttt <malunka221988@gmail.com>
2023-05-25 17:44:08 +0300

    part2

---
+++ src/app/modules/cars/pages/car-edit-page/car-edit-page.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {CarEditFormComponent} from "../../components/car-edit-form/car-edit-form.component";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-car-edit-page',
+  template: '',
+  styleUrls: ['./car-edit-page.component.css']
+})
+export class CarEditPageComponent implements  OnInit{
+  constructor(private matDialog:MatDialog, private activatedRoute : ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(({data})=>{
+      console.log(data)
+      this.matDialog.open(CarEditFormComponent, ({data}))
+
+    })
+  }
+
+}
